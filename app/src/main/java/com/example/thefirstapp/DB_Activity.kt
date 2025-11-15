edcc4ff4339b672729f0adb79928a598b8e40677
friendly2009 <kirill.tsyganov@internet.ru>
2025-11-15 18:23:10 +0300

    добавлен обратный переход а так же исправлено несколько багов

--- app/src/main/java/com/example/thefirstapp/DB_Activity.kt
+++ app/src/main/java/com/example/thefirstapp/DB_Activity.kt
@@ -1,6 +1,6 @@
 package com.example.thefirstapp
 
-import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -11,17 +11,19 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class DB_Activity : AppCompatActivity(){
-    @SuppressLint("MissingSuperCall")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.adminpanel);
-        super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.AdminMain)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
         val button = findViewById<Button>(R.id.ReturnBack);
+        button.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/example/thefirstapp/MainActivity.kt
+++ app/src/main/java/com/example/thefirstapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.thefirstapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -12,20 +13,24 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        enableEdgeToEdge()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val Admin = findViewById<Button>(R.id.Default);
-        val InputAdmin = findViewById<EditText>(R.id.editText)
-        Admin.setOnClickListener {
-            if(InputAdmin.text.toString() == "19614141"){
-                setContentView(R.layout.adminpanel);
-            }
+        val admin = findViewById<Button>(R.id.ButtonAdmin)
+        val InputAdmin = findViewById<EditText>(R.id.editText);
+        admin.setOnClickListener{
+           if(InputAdmin.text.toString() == "19614141"){
+               Toast.makeText(this, "Вы вошли как админ", Toast.LENGTH_SHORT).show()
+               val intent = Intent(this, DB_Activity::class.java)
+               startActivity(intent);
+           }else{
+               Toast.makeText(this, "Неверный пароль", Toast.LENGTH_SHORT).show()
+               InputAdmin.text = null;
+           }
         }
-
     }
 }
